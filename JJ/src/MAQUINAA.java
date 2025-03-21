1c850bd6beabd67f90d4e47a546c54a91b02269c
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2025-03-21 13:12:51 -0300

    maquina a-perro

---
+++ JJ/src/MAQUINAA.java
@@ -0,0 +1,8 @@
+public class MAQUINAA {public class Main {
+    public static void main(String[] args) {
+        Perro perro = new Perro("Perro");
+        perro.ladrar();
+        perro.respondeA(perro.getNombre());
+    }
+}
+}
---
+++ JJ/src/Perro.java
@@ -0,0 +1,22 @@
+
+    public class Perro {
+        private String nombre;
+
+        public Perro(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return this.nombre;
+        }
+
+        public void ladrar() {
+            System.out.println("wof");
+        }
+
+        public boolean respondeA(String unNombre) {
+            System.out.println(nombre.equals(unNombre));
+            return true;
+        }
+    }
+
